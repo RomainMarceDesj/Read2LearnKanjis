7821fed5423767787ab94d3c10843640487398d5
RomainMarceDesj <desjonqueres@keio.jp>
2025-12-05 20:00:35 +0900

    app version + backend fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 import axios from 'axios';
 
 const MemoizedWord = React.memo(Word);
-const API_BASE = "http://127.0.0.1:5000"; // http://127.0.0.1:5000 (local) https://furiganaapi-production.up.railway.app
+const API_BASE = "https://furiganaapi-production.up.railway.app"; // http://127.0.0.1:5000 (local) https://furiganaapi-production.up.railway.app
 const APP_VERSION = "V0App";  // ✅ Version identifier for App.jsx
 
 
